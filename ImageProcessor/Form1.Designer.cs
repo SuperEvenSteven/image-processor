c1123dfcf4bd89aa6cdec172693fae4c2c55305c
Stephen O'Hair - Surface <stephen.ohair@gmail.com>
2017-10-16 20:43:10 +1100

    initial commit

---
+++ ImageProcessor/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace ImageProcessor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1550, 164);
+            this.panel1.TabIndex = 1;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(42, 29);
+            this.button7.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 44);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Process all";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(798, 87);
+            this.button6.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 44);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Test4";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(796, 19);
+            this.button5.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 44);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Test3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(586, 88);
+            this.button4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 40);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Test 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1004, 88);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(184, 31);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "cmu_";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1002, 19);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "RenameFiles";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(586, 19);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Test1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1364, 21);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 179);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1168, 810);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1230, 727);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(324, 277);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(1219, 179);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 150);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operators";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(26, 41);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(95, 29);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "sobel";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(26, 77);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(100, 29);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "kirsch";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1578, 1027);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ ImageProcessor/Form1.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageProcessor {
+    public partial class Form1 : Form {
+
+        //string dirTestFaceBmp = Directory.GetCurrentDirectory() +  @"\data\TestFaceBmp\";
+        //string dirTestNotFaceBmp = Directory.GetCurrentDirectory() + @"\data\TestNotFaceBmp\";
+        string dirTrainFaceBmp = Directory.GetCurrentDirectory() + @"\data\TrainFaceBmp\";
+        string dirTrainNotFaceBmp = Directory.GetCurrentDirectory() + @"\data\TrainNotFaceBmp\";
+        string dirTrainNotFaceBmpOld = Directory.GetCurrentDirectory() + @"\data\TrainNotFaceBmpOld\";
+
+        //string prefix = "cmu_";
+
+        //int testFace = 471;
+        // int testNotFace = 23572;
+        // int trainFace = 2429;
+        // int trainNotFace = 4547;
+
+        Bitmap bmp = null;
+
+        public int[,] sobelX = new int[3, 3] { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+        public int[,] sobelY = new int[3, 3] { { -1, -2, -1 }, { 0, 0, 0 }, { 1, 2, 1 } };
+
+        public int[,] kirschX = new int[3, 3] { { -5, 5, 3 }, { -5, 0, 3 }, { 3, 3, 3 } };
+        public int[,] kirschY = new int[3, 3] { { -5, -5, 3 }, { -5, 0, 3 }, { 3, 3, 3 } };
+
+        public int[,] operatorX;
+        public int[,] operatorY;
+        public string operatorXName;
+        public string operatorYName;
+
+        public int[,] sXtemp = new int[3, 3];
+        public int[,] sYtemp = new int[3, 3];
+
+        double sX = 0;
+        double sY = 0;
+        double mathOperator = 0;
+
+        int[] gaps = new int[7] { 0, 3, 6, 8, 10, 13, 16 };
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void button1_Click_1(object sender, EventArgs e) {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            // test 1
+            bmp = new Bitmap(dirTrainFaceBmp + "face00001.bmp", false);
+
+            pictureBox1.Image = bmp;
+
+            textBox1.Clear();
+
+            string s;
+
+            for (int y = 0; y < 19; y++) {
+                for (int x = 0; x < 19; x++) {
+                    Color c = bmp.GetPixel(x, y);
+                    s = "R" + c.R.ToString() + " " + "G" + c.G.ToString() + " " + "B" + c.B.ToString() + " ";
+                    textBox1.AppendText(s);
+                }
+                textBox1.AppendText("\r\n");
+            }
+
+        }
+
+        public void copyFile(string fromFile, string toFile, int i) {
+            //textBox1.Text = textBox1.Text + "from >>" + fromFile + "\r\n";
+            //textBox1.Text = textBox1.Text + "to   >>" + toFile + "\r\n\r\n";            
+            string m1 = "from >>" + fromFile + "\r\n";
+            string m2 = "to   >>" + toFile + "\r\n\r\n";
+            textBox1.AppendText(m1);
+            textBox1.AppendText(m2);
+
+            //File.Copy(fromFile, toFile, true); // - basically commented out as a saftey catch
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            // Rename Files in dirTrainNotFaceBmpOld 
+
+            string[] filePaths = Directory.GetFiles(dirTrainNotFaceBmpOld);
+
+            textBox1.Clear();
+            for (int i = 0; i < filePaths.Length; i++) {
+
+                //textBox1.Text = textBox1.Text + filePaths[i] + "\r\n";
+                string ff = filePaths[i];
+                string f = Path.GetFileName(ff);
+                //textBox1.Text = textBox1.Text + f + "\r\n";
+
+
+                string fromFile = dirTrainNotFaceBmpOld + f;
+                string toFile = dirTrainNotFaceBmp + textBox2.Text + i.ToString("D5") + ".bmp";
+                // copyFile(fromFile, toFile);
+
+                //if (i > 10) break;
+                copyFile(fromFile, toFile, i);
+
+            }
+        }
+
+        /// <summary>
+        /// multiply (not a matrix multiply) two 3x3 matrixes
+        /// put result back in a
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void threeBythreeMultiply(int[,] a, int[,] b) {
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 3; x++) {
+                    a[x, y] = a[x, y] * b[x, y];
+                }
+            }
+        }
+
+        public int threeBythreeSum(int[,] a) {
+            int sum = 0;
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 3; x++) {
+                    sum = sum + a[x, y];
+                }
+            }
+            return sum;
+        }
+
+        public string threebythreeToString(int[,] a, string heading) {
+            string t = "";
+            if (heading != "") t = t + heading + "\r\n";
+            for (int y = 0; y < 3; y++) {
+                t = t + a[0, y].ToString() + " " + a[1, y].ToString() + " " + a[2, y].ToString() + "\r\n";
+            }
+            return t;
+        }
+
+        double computeOperator(int xx, int yy, Bitmap bmp, bool show) {
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 3; x++) {
+                    Color c = bmp.GetPixel(x + xx, y + yy);
+                    sXtemp[x, y] = c.R;
+                    sYtemp[x, y] = c.R;
+                }
+            }
+
+            if (show) {
+                textBox1.AppendText(threebythreeToString(sXtemp, "sXtemp"));
+                textBox1.AppendText(threebythreeToString(sYtemp, "sYtemp"));
+
+                textBox1.AppendText(threebythreeToString(operatorX, operatorXName));
+                textBox1.AppendText(threebythreeToString(operatorY, operatorYName));
+            }
+            threeBythreeMultiply(sXtemp, operatorX);
+            threeBythreeMultiply(sYtemp, operatorY);
+
+
+            sX = threeBythreeSum(sXtemp);
+            sY = threeBythreeSum(sYtemp);
+            mathOperator = Math.Abs(sX) + Math.Abs(sY);
+            if (show) {
+                textBox1.AppendText(threebythreeToString(sXtemp, "sXtemp"));
+                textBox1.AppendText(threebythreeToString(sYtemp, "sYtemp"));
+                textBox1.AppendText("sX=" + sX.ToString() + " " + "sY=" + sY.ToString() + " operator=" + mathOperator.ToString());
+            }
+            return mathOperator;
+        }
+
+        public string showBitmap(Bitmap bmp) {
+            string s = "";
+
+            for (int y = 0; y < 19; y++) {
+                for (int x = 0; x < 19; x++) {
+                    Color c = bmp.GetPixel(x, y);
+                    string ss = c.R.ToString() + " ";
+                    s = s + ss;
+                }
+                s = s + "\r\n";
+            }
+            return s;
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            // test 2
+            bmp = new Bitmap(dirTrainFaceBmp + "face00001.bmp", false);
+
+            pictureBox1.Image = bmp;
+
+            textBox1.Clear();
+
+            string bb = showBitmap(bmp);
+            textBox1.AppendText(bb);
+
+            // compute sobel @ 0,0
+
+            computeOperator(0, 0, bmp, true);
+
+            textBox1.AppendText("\r\n  .........................................  \r\n");
+
+            computeOperator(3, 3, bmp, true);
+
+            textBox1.AppendText("\r\n  .........................................  \r\n");
+
+            computeOperator(6, 3, bmp, true);
+
+        }
+
+        private void button8_Click(object sender, EventArgs e) {
+            // test 2
+            bmp = new Bitmap(dirTrainFaceBmp + "face00001.bmp", false);
+
+            pictureBox1.Image = bmp;
+
+            textBox1.Clear();
+
+            string bb = showBitmap(bmp);
+            textBox1.AppendText(bb);
+
+            // compute kirsch @ 0,0
+
+            computeOperator(0, 0, bmp, true);
+
+            textBox1.AppendText("\r\n  .........................................  \r\n");
+
+            computeOperator(3, 3, bmp, true);
+
+            textBox1.AppendText("\r\n  .........................................  \r\n");
+
+            computeOperator(6, 3, bmp, true);
+
+        }
+
+        public string processImage(string fileName, int classification, bool show) {
+            string retv = "";
+
+            bmp = new Bitmap(fileName, false);
+
+            pictureBox1.Image = bmp;
+
+            if (show) {
+
+                textBox1.AppendText("\r\n  .#.....................................#.  \r\n");
+                textBox1.AppendText("Fname = " + fileName + "\r\n");
+                string bb = showBitmap(bmp);
+                textBox1.AppendText(bb);
+                textBox1.AppendText("  .-.....................................-.  \r\n");
+            }
+
+            for (int yy = 0; yy < 7; yy++) {
+                for (int xx = 0; xx < 7; xx++) {
+                    double s = computeOperator(gaps[xx], gaps[yy], bmp, false);
+                    //if (show) { textBox1.AppendText(s.ToString() + " "); }
+                    retv = retv + s.ToString() + " ";
+                }
+            }
+            retv = retv + classification.ToString();
+            if (show) { textBox1.AppendText(retv); }
+            return retv;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            // test3
+            textBox1.Clear();
+
+            string fname = dirTrainFaceBmp + "face00001.bmp";
+            string val = processImage(fname, 0, true);
+
+            string fname2 = dirTrainFaceBmp + "face00002.bmp";
+            string val2 = processImage(fname2, 0, true);
+
+            //bmp = new Bitmap(dirTrainFaceBmp + "face00001.bmp", false);
+
+            //pictureBox1.Image = bmp;
+
+            //textBox1.Clear();
+
+            //string bb = showBitmap(bmp);
+            //textBox1.AppendText(bb);
+
+            //textBox1.AppendText("\r\n  .........................................  \r\n");
+
+
+            //for (int yy = 0; yy < 7; yy++)
+            //{
+            //    for (int xx = 0; xx < 7; xx++)
+            //    {
+            //        double s = computeSobel(gaps[xx], gaps[yy], bmp, false);
+            //        textBox1.AppendText(s.ToString() + " ");
+            //    }
+            //}
+
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+            processDir(dirTrainFaceBmp, "face", "D5", 1, 2429, "trainDataFace.txt", 1, true);
+        }
+
+        public void processDir(string inputDirectory, string prefix, string formatS, int lowVal, int hiVal, string outputFile, int classification, bool show) {
+            // test 4 process a directory
+
+            //string inputDirectory = dirTrainFaceBmp;
+            //string prefix = "face";
+            //string formatS = "D5";
+            //int lowVal = 1;
+            //int hiVal = 2429;
+            //string outputFile = "trainDataFace.txt";
+
+            textBox1.Clear();
+
+            string outName = inputDirectory + outputFile;
+            System.IO.StreamWriter file = new System.IO.StreamWriter(outName);
+            //file.WriteLine(lines);
+
+
+            for (int i = lowVal; i <= hiVal; i++) {
+                string fname = inputDirectory + prefix + i.ToString(formatS) + ".bmp";
+                textBox1.AppendText("Process> " + fname + " into " + outName + "\r\n");
+
+                string val = processImage(fname, classification, false);
+
+                if (i < 10 && show) {
+                    textBox1.AppendText(val + "\r\n");
+                }
+                //if (i == 10) break; // debug exit
+
+                file.WriteLine(val);
+            }
+
+            file.Close();
+
+        }
+
+        private void button7_Click(object sender, EventArgs e) {
+            // commented out as a safety catch 
+            // processDir(dirTrainFaceBmp, "face", "D5", 1, 2429, "trainDataFace.txt", 1, true);
+
+            //  processDir(dirTrainNotFaceBmp, "cmu_", "D5", 0, 4547, "trainDataNotFace.txt", 0, true);
+
+            //  processDir(dirTestFaceBmp, "cmu_", "D4", 0, 471, "testDataFace.txt", 1, true);
+
+            //  processDir(dirTestNotFaceBmp, "cmu_", "D4", 0, 23572, "testDataNotFace.txt", 0, true);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) {
+            // sobel operator radio button clicked
+            operatorX = sobelX;
+            operatorY = sobelY;
+            operatorXName = "sobelX";
+            operatorYName = "sobelY";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e) {
+            // kirsch operator radio button clicked
+            operatorX = kirschX;
+            operatorY = kirschY;
+            operatorXName = "kirschX";
+            operatorYName = "kirschY";
+        }
+    }
+}
